ae91b7cbd4893933687d9698aba28a06de79e549
Mateusz Sołtysik <mateusz@soltysik.org>
2016-03-18 22:47:51 +0100

    Work in progress; initial routing

---
+++ www/js/controllers/login.controller.js
@@ -0,0 +1,5 @@
+angular.module('cashmate', ['ionic'])
+  .controller("LoginController", function ($scope) {
+    $scope.custom_value = 44;
+  });
+
---
+++ www/js/controllers/register.controller.js
@@ -0,0 +1,5 @@
+angular.module('cashmate', ['ionic'])
+  .controller("RegisterController", function ($scope) {
+    $scope.custom_value = 44;
+  });
+
