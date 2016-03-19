0032f130e0c11385b54879b5dad40dd80f52fe44
Piotr Staniów <staniowp@gmail.com>
2016-03-19 04:34:20 +0100

    Testing service

--- www/js/controllers/login.controller.js
+++ www/js/controllers/login.controller.js
@@ -2,9 +2,22 @@
   "use strict";
   angular.module('cashmate')
     .controller("LoginController",
-      function ($scope, UserService, $ionicPopup, $state) {
+      function ($scope, UserService, $ionicPopup, $state, $http) {
         $scope.data = {};
+        $scope.pinger = function() {
+          $http.get('http://jupblb.eu/').then(function(r){
+            $ionicPopup.alert({
+              title: 'TEST OK!',
+              template: JSON.stringify(r.data)
+            });
+          }, function(e){
 
+            $ionicPopup.alert({
+              title: 'TEST FAIL!',
+              template: JSON.stringify(e)
+            });
+          });
+        };
         $scope.login = function () {
           console.log('noelo');
           UserService.login($scope.data.username, $scope.data.password)
