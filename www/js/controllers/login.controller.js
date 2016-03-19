9f9603842dbc998e4f495325abe749a979c60e0a
Piotr Staniów <staniowp@gmail.com>
2016-03-19 02:36:35 +0100

    Testing config

--- www/js/controllers/login.controller.js
+++ www/js/controllers/login.controller.js
@@ -4,17 +4,17 @@
     .controller("LoginController",
       function ($scope, UserService, $ionicPopup, $state) {
         $scope.data = {};
-        
+
         $scope.login = function () {
           console.log('noelo');
           UserService.login($scope.data.username, $scope.data.password)
             .then(function (data) {
               console.log(data);
               $state.go('dashboard');
-            }).catch(function () {
+            }).catch(function (errorMsg) {
             $ionicPopup.alert({
               title: 'Login failed!',
-              template: 'Please check your credentials!'
+              template: errorMsg + 'Please check your credentials!'
             });
           });
         }
